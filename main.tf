2d90ad16d288fb58d485951d4cb7aa7c9218eca8
PratyushGR <pratyushrg@gmail.com>
2024-01-19 15:50:18 -0500

    Lambda IAM role for cognitor user authentication

--- main.tf
+++ main.tf
@@ -9,3 +9,9 @@ module "cognito_user_pool" {
   SNS_email_id = var.SNS_email
   # cognito_client_name      = var.user_pool_name
 }
+
+module "Cognito_lambda_IAM_roles" {
+  source = "./modules/Cognito_lambda_IAM_roles"
+
+  cognito_user_pool_id         = module.cognito_user_pool.cognito_user_pool_id
+}
