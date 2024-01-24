9a0df9df45057e6037ece3e0bcefa570c9077a43
PratyushGR <pratyushrg@gmail.com>
2024-01-24 03:05:15 -0500

    cofiguring new lambda runs and post runs API

--- modules/API_GATE_WAY/runs/main.tf
+++ modules/API_GATE_WAY/runs/main.tf
@@ -83,3 +83,62 @@ resource "aws_api_gateway_integration_response" "runs_integration_response_1" {
     "application/json" = ""  # Adjust this based on your response template
   }
 }
+
+
+# Runs Post method
+
+resource "aws_api_gateway_method" "post_method" {
+  rest_api_id   = var.rest_api_id
+  resource_id   = aws_api_gateway_resource.runs.id
+  http_method   = "POST"
+  authorization = "COGNITO_USER_POOLS"
+  authorizer_id = var.authorization
+}
+
+resource "aws_api_gateway_integration" "lambda_integration_1" {
+  rest_api_id = var.rest_api_id
+  resource_id = aws_api_gateway_resource.runs.id
+  http_method = aws_api_gateway_method.post_method.http_method
+  type                    = "AWS"
+  integration_http_method = "POST"
+  uri                     = var.lambda_invoke_arn
+  credentials = var.API_gateway_lamda_runs_arn
+
+  request_templates = {
+    "application/json" = <<EOT
+{
+    "httpMethod": "$context.httpMethod",
+    "userEmail": "$context.authorizer.claims.email",
+    "resourceName": "$context.resourcePath"
+}
+EOT
+  }
+}
+
+resource "aws_api_gateway_method_response" "runs_response_2" {
+  rest_api_id = var.rest_api_id
+  resource_id = aws_api_gateway_resource.runs.id
+  http_method = aws_api_gateway_method.post_method.http_method
+  status_code = 200
+
+  response_parameters = {
+    "method.response.header.Content-Type" = "true",
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+
+  response_models = {
+    "application/json" = "Empty"  # Adjust this based on your response model
+  }
+}
+
+resource "aws_api_gateway_integration_response" "runs_integration_response_2" {
+  rest_api_id = var.rest_api_id
+  resource_id = aws_api_gateway_resource.runs.id
+  http_method = aws_api_gateway_method.post_method.http_method
+  status_code = aws_api_gateway_method_response.runs_response_2.status_code
+
+
+  response_templates = {
+    "application/json" = ""  # Adjust this based on your response template
+  }
+}
